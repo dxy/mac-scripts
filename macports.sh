9ae22ab3f0a031a1e234f0d14554f6f91b609bae
Daisuke Yabuki <dxy@acm.org>
2009-07-20 10:39:08 +0900

    check port command's existence

--- macports.sh
+++ macports.sh
@@ -3,6 +3,7 @@
 
 PATH=/opt/local/bin:/usr/bin:/bin
 
+PORT_COMMAND_PATH=/opt/local/bin/port
 PACKAGE_LIST=( \
     'elinks +col256 +lua' \
     'git-core +bash_completion +doc' \
@@ -13,6 +14,11 @@ PACKAGE_LIST=( \
     'python26' \
     'py-lint'
 )
+
+if [[ ! -x ${PORT_COMMAND_PATH} ]]; then
+  echo 'Unable to locate port command in default location.' 1>&2
+  exit 1
+fi
 
 if [[ ${EUID} -ne 0 ]]; then
   echo 'This script must be run as root.' 1>&2
