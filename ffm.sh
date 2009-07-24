f3f6c5e033938cf8e971ab28d2a2b653981f5e27
dxy <dxy@acm.org>
2009-07-24 10:12:25 +0900

    add ffm.sh for terminal.app and x11 on mac.

---
+++ ffm.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Enable focus-follows-mouse on some applications which support
+# the functionality (e.g. Terminal.app).
+
+format_version() {
+  # "Apple has a formalised version number structure based around the NumVersion
+  # struct, which specifies a one- or two-digit major version, a one-digit minor
+  # version, a one-digit "bug" (i.e. revision) version, a stage indicator (drawn
+  # from the set development/prealpha, alpha, beta and final/release), and a
+  # one-byte (i.e. having values in the range 0-255) pre-release version, which
+  # is only used at stages prior to final."
+  # cf. http://en.wikipedia.org/wiki/Software_versioning
+
+  local MAJOR_VER=$(sw_vers -productVersion | cut -d. -f1)
+  local MINOR_VER=$(sw_vers -productVersion | cut -d. -f2)
+  local BUG_VER=$(sw_vers -productVersion | cut -d. -f3)
+
+  echo $(printf "%02d%1d%1d" ${MAJOR_VER} ${MINOR_VER} ${BUG_VER})
+}
+
+### Terminal.app
+defaults write com.apple.Terminal FocusFollowsMouse -string YES
+
+### X11
+# cf. http://stackoverflow.com/questions/98310/focus-follows-mouse-plus-auto-raise-on-mac-os-x
+# The following code for ffm on x11 is as much a reminder for myself as 
+# anything. I haven't tested it since I don't really need it.
+
+FORMATTED_VER=$(format_version)
+if [[ ${FORMATTED_VER} -le 1055 ]]; then 
+  # for version previous to 10.5.5
+  defaults write com.apple.x11 wm_ffm true
+else
+  defaults write org.x.x11 wm_ffm true
+fi
+
