2fe9deddc483d4eb6e758f510ef684ad17c13fa7
dxy <dxy@acm.org>
2009-07-28 08:36:39 +0900

    support port command installed in non-default location.

--- macports.sh
+++ macports.sh
@@ -3,7 +3,6 @@
 
 PATH=/opt/local/bin:/usr/bin:/bin
 
-PORT_COMMAND_PATH=/opt/local/bin/port
 PACKAGE_LIST=( 
     'elinks +col256'
     'erlang +ssl'
@@ -33,8 +32,8 @@ PACKAGE_LIST=(
     'sox'
 )
 
-if [[ ! -x ${PORT_COMMAND_PATH} ]]; then
-  echo 'Unable to locate port command in default location.' 1>&2
+if [[ ! -x $(which port) ]]; then
+  echo 'Unable to locate port command.' 1>&2
   exit 1
 fi
 
